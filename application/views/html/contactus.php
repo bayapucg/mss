fe0a09d838284b0547e22debfecd279d64670d53
SathwikGunda <42731745+SathwikGunda@users.noreply.github.com>
2018-10-17 16:37:09 +0530

    commit

--- application/views/html/contactus.php
+++ application/views/html/contactus.php
@@ -162,6 +162,6 @@
     ===== Map Section Start  ===========
     ===================================-->
 <!-- Map Section Start -->
-<section class="section-padding">
+<section class="">
     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3806.7086128290966!2d78.51254571416538!3d17.42576598805563!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bcb998693ffffff%3A0x460ae922953c625c!2sNehru+High+School!5e0!3m2!1sen!2sin!4v1538215168863" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
 </section>
